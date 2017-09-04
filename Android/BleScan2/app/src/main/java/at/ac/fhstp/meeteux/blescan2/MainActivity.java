87fbb94ea1888b96d92b80194c35a8b86894462e
Laura Breban <Breban.Laura@gmail.com>
2017-09-04 09:55:28 +0200

    scanning every second

--- Android/BleScan2/app/src/main/java/at/ac/fhstp/meeteux/blescan2/MainActivity.java
+++ Android/BleScan2/app/src/main/java/at/ac/fhstp/meeteux/blescan2/MainActivity.java
@@ -2,13 +2,17 @@ package at.ac.fhstp.meeteux.blescan2;
 
 
 import android.Manifest;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.kontakt.sdk.android.ble.configuration.ActivityCheckConfiguration;
 import com.kontakt.sdk.android.ble.connection.OnServiceReadyListener;
 import com.kontakt.sdk.android.ble.device.BeaconRegion;
 import com.kontakt.sdk.android.ble.manager.ProximityManager;
@@ -21,6 +25,12 @@ import com.kontakt.sdk.android.common.KontaktSDK;
 import com.kontakt.sdk.android.common.profile.IBeaconDevice;
 import com.kontakt.sdk.android.common.profile.IBeaconRegion;
 
+
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+
+import org.altbeacon.beacon.*;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -28,24 +38,28 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
 
-import okhttp3.Headers;
-
-import static android.R.id.list;
-import static at.ac.fhstp.meeteux.blescan2.R.id.listView;
-
 public class MainActivity extends AbsRuntimePermission {
     private static final int REQUEST_PERMISSION = 10;
     private ProximityManager proximityManager;
+    private BackgroundPowerSaver backgroundPowerSaver;
+    private BeaconManager beaconManager;
 
 
     ListView listView;
     String[] beaconItems;
 
+    private static final Object SINGLETON_LOCK = new Object();
+    protected static volatile BeaconManager sInstance = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        backgroundPowerSaver = new BackgroundPowerSaver(this);
         setContentView(R.layout.activity_main);
+        beaconManager = BeaconManager.getInstanceForApplication(getApplicationContext());
+
+
 
         requestAppPermissions(new String[]{
                 Manifest.permission.ACCESS_FINE_LOCATION,
@@ -58,6 +72,8 @@ public class MainActivity extends AbsRuntimePermission {
                 REQUEST_PERMISSION);
 
         listView = (ListView) findViewById(R.id.listView);
+
+
 
 
 
@@ -83,7 +99,8 @@ public class MainActivity extends AbsRuntimePermission {
         beaconRegions.add(region1);
 
         proximityManager.spaces().iBeaconRegions(beaconRegions);
-
+      //  proximityManager.configuration().activityCheckConfiguration(ActivityCheckConfiguration.MINIMAL);
+        proximityManager.configuration().deviceUpdateCallbackInterval(1000);
 
         /* proximityManager.setIBeaconListener(createIBeaconListener()); */
 
@@ -94,6 +111,15 @@ public class MainActivity extends AbsRuntimePermission {
                 Log.i("Sample", "Beacon discovered");
                 Log.i("Sample", "IBeacon discovered: " + iBeacon.toString());
 
+
+            }
+
+            private ScanSettings getScanSettings()
+            {
+                final ScanSettings.Builder builder = new ScanSettings.Builder();
+                builder.setReportDelay(0);
+                builder.setScanMode(ScanSettings.SCAN_MODE_LOW_POWER);
+                return builder.build();
             }
 
             @Override
@@ -101,6 +127,7 @@ public class MainActivity extends AbsRuntimePermission {
                 //Beacons updated
                 Log.i("Sample", "Beacon updated");
                 Log.i("Sample", "IBeacon updated: " + iBeacons.toString());
+
                 //readBeaconData(iBeacons);
                 beaconItems = new String[iBeacons.size()];
                 List<IBeaconDevice> newList = new ArrayList<>(iBeacons);
@@ -140,7 +167,7 @@ public class MainActivity extends AbsRuntimePermission {
             }
         });
 
-        proximityManager.setScanStatusListener(createScanStatusListener());
+         proximityManager.setScanStatusListener(createScanStatusListener());
 
     }
 
@@ -150,6 +177,8 @@ public class MainActivity extends AbsRuntimePermission {
 
         Log.i("Sample", "Start scanning");
         startScanning();
+
+
     }
 
     @Override
@@ -209,6 +238,33 @@ public class MainActivity extends AbsRuntimePermission {
             }
         };
     }
+
+ /*   @NonNull
+    public static BeaconManager getInstanceForApplication(@NonNull Context context) {
+        /*
+         * Follow double check pattern from Effective Java v2 Item 71.
+         *
+         * Bloch recommends using the local variable for this for performance reasons:
+         *
+         * > What this variable does is ensure that `field` is read only once in the common case
+         * > where it's already initialized. While not strictly necessary, this may improve
+         * > performance and is more elegant by the standards applied to low-level concurrent
+         * > programming. On my machine, [this] is about 25 percent faster than the obvious
+         * > version without a local variable.
+         *
+         * Joshua Bloch. Effective Java, Second Edition. Addison-Wesley, 2008. pages 283-284
+         */
+  /*      BeaconManager instance = sInstance;
+        if (instance == null) {
+            synchronized (SINGLETON_LOCK) {
+                instance = sInstance;
+                if (instance == null) {
+                    sInstance = instance = new org.altbeacon.beacon.BeaconManager(context);
+                }
+            }
+        }
+        return instance;
+    } */
 
 
 }
