ffcdea20f92437c538bfc2fa3966fe5e58226be2
Kerstin Blumenstein <kblumenstein@fhstp.ac.at>
2018-12-12 23:09:33 +0100

    adds first implementation of genvis > shows json data with birth, death, marriage

---
+++ WWW/genvis/genealogy-data.js
@@ -0,0 +1,244 @@
+var persons = [{
+    "name": "Leopold III. (13)",
+    "died": 1136,
+    "born": 1073,
+    "gender": "man",
+    "marriages": [{
+        "spouse": {
+            "name": "? v. Perg (N)",
+            "gender": "woman",
+            "died": 1136,
+            "born": 1073,
+        },
+        "children": [{
+            "name": "Adalbert (14)",
+            "born": 1120,
+            "died": 1138,
+            "gender": "woman",
+            "marriages": [{
+                "spouse": {
+                    "name": "Adelheid (N)",
+                    "gender": "woman",
+                    "born": 1120,
+                    "died": 1138,
+                }
+            },{
+                "spouse": {
+                    "name": "Sophie (55)",
+                    "gender": "woman",
+                    "born": 1120,
+                    "died": 1138,
+                }
+            }]
+        }]
+    },{
+        "spouse": {
+            "name": "Agnes (44)",
+            "gender": "woman",
+            "died": 1177,
+            "born": 1107,
+        },
+        "children": [{
+            "name": "Heinrich II. (19)",
+            "died": 1177,
+            "born": 1107,
+            "gender": "man",
+            "marriages": [{
+                "spouse": {
+                    "name": "Gertrud (56)",
+                    "born": 1100,
+                    "died": 1143,
+                    "gender": "woman",
+                },
+                "children": [{
+                    "name": "Richardis (N)",
+                    "born": 1100,
+                    "died": 1185,
+                    "gender": "woman"
+                }]
+            },{
+                "spouse": {
+                    "name": "Theodora (57)",
+                    "born": 1100,
+                    "died": 1183,
+                    "gender": "woman"
+                },
+                "children": [{
+                    "name": "Agnes (58)",
+                    "born": 1100,
+                    "died": 1184,
+                    "gender": "woman"
+                },{
+                    "name": "Leopold V. (20)",
+                    "died": 1194,
+                    "born": 1157,
+                    "gender": "man",
+                    "marriages": [{
+                        "spouse": {
+                            "name": "Helene (60)",
+                            "gender": "woman",
+                            "born" : 1150,
+                            "died" : 1200,
+                        },
+                        "children": [{
+                            "name": "Friedrich I. (23)",
+                            "died": 1198,
+                            "born": 1175,
+                            "gender": "man"
+                        },{
+                            "name": "Leopold IV. (24)",
+                            "died": 1230,
+                            "born": 1176,
+                            "gender": "man",
+                            "marriages": [{
+                                "spouse": {
+                                    "name": "Theodora (63)",
+                                    "born": 1200,
+                                    "died": 1246,
+                                    "gender": "woman",
+                                },
+                                "children": [{
+                                    "name": "Margarethe (64)",
+                                    "born": 1200,
+                                    "died": 1266,
+                                    "gender": "woman",
+                                },{
+                                    "name": "Agnes (65)",
+                                    "born": 1200,
+                                    "died": 1226,
+                                    "gender": "woman",
+                                },{
+                                    "name": "Leopold (25)",
+                                    "born": 1200,
+                                    "died": 1216,
+                                    "gender": "man"
+                                },{
+                                    "name": "Heinrich (26)",
+                                    "born": 1200,
+                                    "died": 1227,
+                                    "gender": "man",
+                                    "marriages": [{
+                                        "spouse": {
+                                            "name": "Agnes (68V)",
+                                            "gender": "woman",
+                                            "born" : 1200,
+                                            "died" : 1250,
+                                        },
+                                        "children" : [{
+                                            "name" : "Gertrud (69)",
+                                            "gender" : "woman",
+                                            "born" : 1200,
+                                            "died" : 1250,
+                                        }]
+                                    }]
+                                },{
+                                    "name": "Friedrich II. (27)",
+                                    "died": 1246,
+                                    "born": 1210,
+                                    "gender": "man",
+                                    "marriages" : [{
+                                        "spouse" : {
+                                            "name" : "Sophie (73V)",
+                                            "gender" : "woman",
+                                            "born" : 1200,
+                                            "died" : 1250,
+                                        }
+                                    },{
+                                        "spouse" : {
+                                            "name" : "Agnes v. Meranien (72)",
+                                            "gender" : "woman",
+                                            "born": 1200,
+                                            "died" : 1260
+                                        }
+                                    }]
+                                },{
+                                    "name": "Konstanze (66)",
+                                    "born": 1200,
+                                    "died": 1243,
+                                    "gender": "woman"
+                                },{
+                                    "name": "Gertrud (67)",
+                                    "born" : 1200,
+                                    "died": 1241,
+                                    "gender": "woman"
+                                }]
+                            }]
+                        },{
+                            "name": "Agnes (61V)",
+                            "gender": "woman",
+                            "born": 1100,
+                            "died": 1120
+                        }]
+                    }]
+                },{
+                    "name": "Heinrich d.Ä. (21)",
+                    "born": 1200,
+                    "died": 1223,
+                    "gender": "man",
+                    "marriages": [{
+                        "spouse": {
+                            "name": "Richza (62)",
+                            "born": 1100,
+                            "died": 1182,
+                            "gender": "woman",
+                        },
+                        "children": [{
+                            "name": "Heinrich d.J. (22)",
+                            "born": 1200,
+                            "died": 1236,
+                            "gender": "man"
+                        }]
+                    }]
+                }]
+            }]
+        },{
+            "name": "Leopold IV. (15)",
+            "died": 1141,
+            "born": 1108,
+            "gender": "man",
+            "marriages": [{
+                "spouse": {
+                    "name": "Maria (59)",
+                    "gender": "woman",
+                    "died": 1141,
+                    "born": 1108,
+                }
+            }]
+        },{
+            "name": "Berta (45)",
+            "born": 1100,
+            "died": 1150,
+            "gender": "woman"
+        },{
+            "name": "Agnes (46)",
+            "born": 1100,
+            "died": 1157,
+            "gender": "woman"
+        },{
+            "name": "Otto (16)",
+            "died": 1158,
+            "born": 1112,
+            "gender": "woman"
+        },{
+            "name": "Konrad (17)",
+            "died": 1168,
+            "born": 1115,
+            "gender": "woman"
+        },{
+            "name": "Elisabeth (48)",
+            "born": 1100,
+            "died": 1143,
+            "gender": "woman"
+        },{
+            "name": "Gertrud (47)",
+            "born": 1100,
+            "died": 1151,
+            "gender": "woman"
+        },{
+            "name": "Judith (49)",
+            "born": 1100,
+            "died": 1191,
+            "gender": "woman"
+        }]
+    }]
+}];
---
+++ WWW/genvis/script2.js
@@ -0,0 +1,416 @@
+var events = [{startYear: "900", endYear: "910", name: "some war"}, {startYear: "920", endYear: "920", name: "some 2nd war"}];
+
+var persons = [{
+    "name": "Leopold III. (13)",
+    "died": 1136,
+    "born": 1073,
+    "class": "man",
+    "marriages": [{
+        "spouse": {
+            "name": "? v. Perg (N)",
+            "class": "woman",
+            "died": 1136,
+            "born": 1073,
+            "marriage": 1085,
+        },
+        "children": [{
+            "name": "Adalbert (14)",
+            "born": 1120,
+            "died": 1138,
+            "class": "woman",
+            "marriages": [{
+                "spouse": {
+                    "name": "Adelheid (N)",
+                    "class": "woman",
+                    "born": 1120,
+                    "died": 1138,
+                    "marriage": 1125,
+                }
+            },{
+                "spouse": {
+                    "name": "Sophie (55)",
+                    "class": "woman",
+                    "born": 1120,
+                    "died": 1138,
+                    "marriage": 1130,
+                }
+            }]
+        }]
+    },{
+        "spouse": {
+            "name": "Agnes (44)",
+            "class": "woman",
+            "died": 1177,
+            "born": 1107,
+            "marriage": 1150,
+        },
+        "children": [{
+            "name": "Heinrich II. (19)",
+            "died": 1177,
+            "born": 1107,
+            "class": "man",
+            "marriages": [{
+                "spouse": {
+                    "name": "Gertrud (56)",
+                    "born": 1100,
+                    "died": 1143,
+                    "class": "woman",
+                    "marriage": 1120,
+                },
+                "children": [{
+                    "name": "Richardis (N)",
+                    "born": 1100,
+                    "died": 1185,
+                    "class": "woman",
+                }]
+            },{
+                "spouse": {
+                    "name": "Theodora (57)",
+                    "born": 1100,
+                    "died": 1183,
+                    "class": "woman",
+                    "marriage": 1160,
+                },
+                "children": [{
+                    "name": "Agnes (58)",
+                    "born": 1100,
+                    "died": 1184,
+                    "class": "woman"
+                },{
+                    "name": "Leopold V. (20)",
+                    "died": 1194,
+                    "born": 1157,
+                    "class": "man",
+                    "marriages": [{
+                        "spouse": {
+                            "name": "Helene (60)",
+                            "class": "woman",
+                            "born" : 1150,
+                            "died" : 1200,
+                            "marriage": 1170,
+                        },
+                        "children": [{
+                            "name": "Friedrich I. (23)",
+                            "died": 1198,
+                            "born": 1175,
+                            "class": "man"
+                        },{
+                            "name": "Leopold IV. (24)",
+                            "died": 1230,
+                            "born": 1176,
+                            "class": "man",
+                            "marriages": [{
+                                "spouse": {
+                                    "name": "Theodora (63)",
+                                    "born": 1200,
+                                    "died": 1246,
+                                    "class": "woman",
+                                    "marriage": 1220,
+                                },
+                                "children": [{
+                                    "name": "Margarethe (64)",
+                                    "born": 1200,
+                                    "died": 1266,
+                                    "class": "woman",
+                                },{
+                                    "name": "Agnes (65)",
+                                    "born": 1200,
+                                    "died": 1226,
+                                    "class": "woman",
+                                },{
+                                    "name": "Leopold (25)",
+                                    "born": 1200,
+                                    "died": 1216,
+                                    "class": "man"
+                                },{
+                                    "name": "Heinrich (26)",
+                                    "born": 1200,
+                                    "died": 1227,
+                                    "class": "man",
+                                    "marriages": [{
+                                        "spouse": {
+                                            "name": "Agnes (68V)",
+                                            "class": "woman",
+                                            "born" : 1200,
+                                            "died" : 1250,
+                                            "marriage": 1210,
+                                        },
+                                        "children" : [{
+                                            "name" : "Gertrud (69)",
+                                            "class" : "woman",
+                                            "born" : 1200,
+                                            "died" : 1250,
+                                        }]
+                                    }]
+                                },{
+                                    "name": "Friedrich II. (27)",
+                                    "died": 1246,
+                                    "born": 1210,
+                                    "class": "man",
+                                    "marriages" : [{
+                                        "spouse" : {
+                                            "name" : "Sophie (73V)",
+                                            "class" : "woman",
+                                            "born" : 1200,
+                                            "died" : 1250,
+                                            "marriage": 1210,
+                                        }
+                                    },{
+                                        "spouse" : {
+                                            "name" : "Agnes v. Meranien (72)",
+                                            "class" : "woman",
+                                            "born": 1200,
+                                            "died" : 1260,
+                                            "marriage": 1230,
+                                        }
+                                    }]
+                                },{
+                                    "name": "Konstanze (66)",
+                                    "born": 1200,
+                                    "died": 1243,
+                                    "class": "woman"
+                                },{
+                                    "name": "Gertrud (67)",
+                                    "born" : 1200,
+                                    "died": 1241,
+                                    "class": "woman"
+                                }]
+                            }]
+                        },{
+                            "name": "Agnes (61V)",
+                            "class": "woman",
+                            "born": 1100,
+                            "died": 1120
+                        }]
+                    }]
+                },{
+                    "name": "Heinrich d.Ä. (21)",
+                    "born": 1150,
+                    "died": 1223,
+                    "class": "man",
+                    "marriages": [{
+                        "spouse": {
+                            "name": "Richza (62)",
+                            "born": 1100,
+                            "died": 1182,
+                            "class": "woman",
+                            "marriage": 1155,
+                        },
+                        "children": [{
+                            "name": "Heinrich d.J. (22)",
+                            "born": 1160,
+                            "died": 1236,
+                            "class": "man"
+                        }]
+                    }]
+                }]
+            }]
+        },{
+            "name": "Leopold IV. (15)",
+            "died": 1141,
+            "born": 1108,
+            "class": "man",
+            "marriages": [{
+                "spouse": {
+                    "name": "Maria (59)",
+                    "class": "woman",
+                    "died": 1141,
+                    "born": 1108,
+                    "marriage": 1120,
+                }
+            }]
+        },{
+            "name": "Berta (45)",
+            "born": 1100,
+            "died": 1150,
+            "class": "woman"
+        },{
+            "name": "Agnes (46)",
+            "born": 1100,
+            "died": 1157,
+            "class": "woman"
+        },{
+            "name": "Otto (16)",
+            "died": 1158,
+            "born": 1112,
+            "class": "woman"
+        },{
+            "name": "Konrad (17)",
+            "died": 1168,
+            "born": 1115,
+            "class": "woman"
+        },{
+            "name": "Elisabeth (48)",
+            "born": 1100,
+            "died": 1143,
+            "class": "woman"
+        },{
+            "name": "Gertrud (47)",
+            "born": 1100,
+            "died": 1151,
+            "class": "woman"
+        },{
+            "name": "Judith (49)",
+            "born": 1100,
+            "died": 1191,
+            "class": "woman"
+        }]
+    }]
+}];
+
+var dataArray = [];
+dataArray[0] = [{x:5, y:5}, {x:10, y:5}, {x:15, y:10}, {x:50, y:10}]; // first: birth, second & third: calculation for mariage, forth: dead
+dataArray[1] = [{x:10, y:25}, {x:15, y:20}, {x:60, y:20}];
+dataArray[2] = [{x:30, y:45}, {x:100, y:45}];
+
+var interpolateTypes = [d3.curveLinear, d3.curveNatural, d3.curveStep, d3.curveBasis, d3.curveBundle, d3.curveCardinal];
+var stringDates = ['1000', '1300'];
+var parseDate = d3.timeParse("%Y");
+
+//var minDate = d3.min(data, function(d){ return d.month; });
+//var maxDate = d3.max(data, function(d){ return d.month; });
+
+//var j = d3.scaleTime().domain(d3.extent(stringDates, function(d){ return parseDate(d); })).range([0,chartWidth]);
+
+var svgHeight = 2000;
+var svgWidth = 2000;
+
+var x = d3.scaleTime()
+.domain(d3.extent(stringDates, function(d){return parseDate(d)}))
+.range([0, svgWidth]); // heighest x * 6
+
+
+// get the x-coordinates depending on year
+var whichX = d3.scaleLinear()
+    .domain([1000, 1300]) // min and max year of data set
+    .range([0, svgWidth]); // min and max of svg
+
+var personArray = [];
+var itterator = 0;
+var startY = 20;
+var marriageXDiff = 5;
+var marriageYDiff = 5;
+var yDiff = 50;
+
+persons.forEach(person => {
+    console.log(person);
+    console.log(person.name + " " + person.born + " " + whichX(person.born));
+    personArray[itterator] = [{x: whichX(person.born), y: startY}, {x: whichX(person.died), y: startY}];
+    startY += yDiff;
+    itterator++;
+    console.log(personArray);
+    isMarried(person);
+});
+
+function isMarried(person){
+    console.log(person);
+
+    if(typeof person.marriages != "undefined"){
+   /*     var marriageX1 = whichX(person.marriages[0].spouse.marriage) - marriageXDiff;
+        var marriageX2 = whichX(person.marriages[0].spouse.marriage) + marriageXDiff;
+        var marriageY = startY + marriageYDiff;
+        personArray[itterator] = [
+            {x: whichX(person.born), y: startY}, 
+            {x: marriageX1, y: startY}, // marriage 1
+            {x: marriageX2, y: marriageY}, // marriage 2
+            {x: whichX(person.died), y: marriageY}
+        ];
+        startY += yDiff;
+        itterator++;*/
+
+        console.log("ist verheiratet");
+        person.marriages.forEach(marriage => {
+            console.log(marriage.spouse.name + " " + marriage.spouse.born + " " + whichX(marriage.spouse.born));
+            // calculate marriage
+            var marriageX1 = whichX(marriage.spouse.marriage) - marriageXDiff;
+            var marriageX2 = whichX(marriage.spouse.marriage) + marriageXDiff;
+            var marriageY = startY - marriageYDiff;
+            
+            personArray[itterator] = [
+                {x: whichX(marriage.spouse.born), y: startY}, // born
+                {x: marriageX1, y: startY}, // marriage 1
+                {x: marriageX2, y: marriageY}, // marriage 2
+                {x: whichX(marriage.spouse.died), y: marriageY}
+            ];
+            
+            startY += yDiff;
+            itterator++;
+            console.log(personArray);
+            if(typeof marriage.children !== "undefined"){
+                console.log("hat Kinder");
+                marriage.children.forEach(child => {
+                    console.log(child);
+                    console.log(child.name + " " + child.born + " " + whichX(child.born));
+                    personArray[itterator] = [{x: whichX(child.born), y: startY}, {x: whichX(child.died), y: startY}];
+                    startY += yDiff;
+                    itterator++;
+                    isMarried(child);
+                });
+            }else{
+                console.log("keine Kinder");
+            }
+        });
+    }else{
+        console.log("nicht verheiratet");
+        return;
+    }
+}
+
+
+console.log('900: ', whichX(900));
+console.log('920: ', whichX(920));
+
+var xAxis = d3.axisBottom(x);
+
+var svg = d3.select("body").append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
+var line = d3.line()
+    .x(function(d, i){ return d.x; })
+    .y(function(d, i){ return d.y; })
+    .curve(d3.curveLinear); // generates a path element which is a line
+
+var chartGroup = svg.append("g")
+    .attr("class", "group")
+    .attr("transform", "translate(0,0)");
+
+var firstGroups = chartGroup.selectAll("g")
+    .data(personArray)
+    .enter().append("g")
+        .attr("class", function(d,i){ return "firstLevelGroup"+i;});
+
+var secondGroups = firstGroups.append("path")
+    .attr("fill", "none")
+    .attr("stroke", "blue")
+    .attr("stroke-width", "25")
+    .attr("d", function(d){ return line(d); });
+
+
+/*
+for(var i=0; i < dataArray.length; i++){
+    firstGroups.selectAll("circle.grp")
+        .data(personArray[i])
+        .enter()
+        .append("text")
+}
+/*
+for(var i=0; i < dataArray.length; i++){
+    firstGroups.selectAll("circle.grp")
+        .data(personArray[i])
+        .enter()
+        .append("circle")
+            .attr("class", function(d, j){ return "grp"+j; })
+            .attr("cx", function(d){ return d.x; })
+            .attr("cy", function(d){ return d.y; })
+            .attr("r", "5")
+            .attr("fill", "yellow");
+}*/
+
+
+var axisGroup = svg.append("svg")
+//d3.select("body").append("svg").attr("height", "100").attr("width", svgWidth)
+//    chartGroup.append("g")
+    .attr("class", "x axis").attr("transform", "translate(0,0)").style("position", "fixed").style("top", "0").style("left", "0")
+    .call(xAxis);
+
